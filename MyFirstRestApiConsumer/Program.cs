57f72468c35ea3ed5e286e21fe8fc751775ca907
GEORGE LITSIOS <litsiosgeorgios@hotmail.com>
2021-01-31 22:14:22 +0200

    2nd commit

--- MyFirstRestApiConsumer/Program.cs
+++ MyFirstRestApiConsumer/Program.cs
@@ -11,12 +11,12 @@ namespace MyFirstRestApiConsumer
         {
             string url = @"https://jsonmock.hackerrank.com/api/countries";
 
-            HttpClient client = new HttpClient();
+           
 
             
-            /* using (var httpClient = new HttpClient())
+             /*using (var httpClient = new HttpClient())
              {
-                 httpClient.DefaultRequestHeaders.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+                 //httpClient.DefaultRequestHeaders.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
                  var response = httpClient.GetStringAsync(new Uri(url)).Result;
 
                  Console.WriteLine(response);
@@ -25,21 +25,27 @@ namespace MyFirstRestApiConsumer
              }*/
 
             // Call asynchronous network methods in a try/catch block to handle exceptions.
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                // string responseBody = await client.GetStringAsync(uri);
 
-                Console.WriteLine(responseBody);
-            }
-            catch (HttpRequestException e)
+
+            using (var client = new HttpClient())
             {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    // Above three lines can be replaced with new helper method below
+                    // string responseBody = await client.GetStringAsync(uri);
+
+                    Console.WriteLine(responseBody);
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
             }
+
 
             Console.ReadLine();
         }
