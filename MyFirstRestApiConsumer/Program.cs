07e0a3b05110f90a2988f99b0c7c31ceb3f1feb1
GEORGE LITSIOS <litsiosgeorgios@hotmail.com>
2021-02-01 00:27:40 +0200

    Working Commit

--- MyFirstRestApiConsumer/Program.cs
+++ MyFirstRestApiConsumer/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+
 
 namespace MyFirstRestApiConsumer
 {
@@ -9,9 +12,18 @@ namespace MyFirstRestApiConsumer
 
         static async Task Main(string[] args)
         {
-            string url = @"https://jsonmock.hackerrank.com/api/countries";
+            string substr1 = "en";
 
-           
+            long MinPopulation = 10000000;
+
+            int currentPage = 1;
+
+            string url = @"https://jsonmock.hackerrank.com/api/countries/search?name="+substr1+@"&page="+currentPage;
+
+            Console.WriteLine(url);
+
+
+            int countCountries = 0;
 
             
              /*using (var httpClient = new HttpClient())
@@ -37,7 +49,38 @@ namespace MyFirstRestApiConsumer
                     // Above three lines can be replaced with new helper method below
                     // string responseBody = await client.GetStringAsync(uri);
 
-                    Console.WriteLine(responseBody);
+                    //var JsonObj = JsonSerializer.Deserialize<dynamic>(responseBody);
+
+                    var JsonObj = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                    int totalPages = JsonObj.total_pages;
+
+                    while(currentPage <= totalPages)
+                    {
+                        foreach(var country in JsonObj.data)
+                        {
+                            if (country.population >= MinPopulation)
+                            {
+                                countCountries++;
+                                Console.WriteLine(country.name);
+                                Console.WriteLine(currentPage);
+                            }
+                        }
+
+                        //Next page
+
+                        currentPage++;
+                        url = @"https://jsonmock.hackerrank.com/api/countries/search?name=" + substr1 + @"&page=" + currentPage;
+
+                        response = await client.GetAsync(url);
+                        response.EnsureSuccessStatusCode();
+                        responseBody = await response.Content.ReadAsStringAsync();
+                        JsonObj = JsonConvert.DeserializeObject<dynamic>(responseBody);
+
+                    }                    
+
+                    
+                    Console.WriteLine($"The total countries whose name contains the string: {substr1} and which have population more than: {MinPopulation} are: {countCountries}");
                 }
                 catch (HttpRequestException e)
                 {
@@ -45,7 +88,6 @@ namespace MyFirstRestApiConsumer
                     Console.WriteLine("Message :{0} ", e.Message);
                 }
             }
-
 
             Console.ReadLine();
         }
